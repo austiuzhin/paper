83a12a8d41698dbeb2bf952b8423a10b23b4d63c
Yuriy Orlov <navuchodonsr@yandex.ru>
2016-11-24 18:11:50 +0300

    no serious changes

--- app/migrations/lat_lng.py
+++ app/migrations/lat_lng.py
@@ -1,5 +1,14 @@
 from d_base import Item
+import googlemaps
 
+
+with open('static/map_api_key.txt','r') as file:
+		api_key = file.readline()
 
 total = Item.query.filter(Item.id).count()
 total_n = Item.query.filter(Item.lat == None).count()
+
+gmaps = googlemaps.Client(key=api_key)
+geocode_result = gmaps.geocode(address_string)
+lat = geocode_result[0].get('geometry').get('location').get('lat')
+lng = geocode_result[0].get('geometry').get('location').get('lng')
